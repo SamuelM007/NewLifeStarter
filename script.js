e661d6d7de106aff4008b4a91a7fa6f68b7a465d
SamuelM007 <samuelmarchuk07@gmail.com>
2025-03-13 21:23:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function volunteer() {
+  window.open("https://bellevuewa.gov/volunteering", "_blank", "", "");
+}
+
+function donate() {
+  window.open(
+    "https://www.gofundme.com/f/8ubzf-homeless-in-seattle?qid=7697ad18dc42d9a5b802cc35bda40ac8",
+    "_blank",
+    "",
+    ""
+  );
+}
+
+function otherWays() {
+  window.open("https://seattlehomeless.org/", "_blank", "", "");
+}
+
+function health() {
+  window.open(
+    "https://kingcounty.gov/en/dept/dph/health-safety/health-centers-programs-services/health-services-for-the-homeless/healthcare-for-the-homeless/services",
+    "_blank",
+    "",
+    ""
+  );
+}
+
+function employment() {
+  window.open(
+    "https://www.ywcaworks.org/programs/homeless-employment-program",
+    "_blank",
+    "",
+    ""
+  );
+}
